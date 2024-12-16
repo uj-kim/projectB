e888c2672c1791302728909fa0205cac3ae11a84
uj-kim <uj-kim@users.noreply.github.com>
2024-12-17 07:41:59 +0900

    test(utils): add and pass all tests for useModal spec and error,not found page

--- src/pages/error/components/__tests__/ErrorPage.spec.jsx
+++ src/pages/error/components/__tests__/ErrorPage.spec.jsx
@@ -3,14 +3,26 @@ import { vi } from 'vitest';
 
 import render from '@/utils/test/render';
 import { ErrorPage } from '../ErrorPage';
+import { useNavigate } from 'react-router-dom';
 
 // 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행 (react-router-dom의 useNavigate 모킹)
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
 
 it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {
   // Arrange: ErrorPage 컴포넌트를 렌더링
+  const navigate = vi.fn();
+  vi.mocked(useNavigate).mockReturnValue(navigate);
   const { user } = await render(<ErrorPage />);
 
   // Act: "뒤로 이동" 버튼을 클릭
+  await user.click(screen.getByText('뒤로 이동'));
 
   // Assert: navigate 함수가 -1 인자로 호출되었는지 확인
+  expect(navigate).toHaveBeenCalledWith(-1);
 });
--- src/pages/error/components/__tests__/NotFoundPage.spec.jsx
+++ src/pages/error/components/__tests__/NotFoundPage.spec.jsx
@@ -3,14 +3,25 @@ import { vi } from 'vitest';
 
 import render from '@/utils/test/render';
 import { NotFoundPage } from '@/pages/error/components/NotFoundPage';
+import { useNavigate } from 'react-router-dom';
 
 // 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행 (react-router-dom의 useNavigate 모킹)
-
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
 it('Home으로 이동 버튼 클릭시 홈 경로로 이동하는 navigate가 실행된다', async () => {
   // Arrange: NotFoundPage 컴포넌트를 렌더링
+  const navigate = vi.fn();
+  vi.mocked(useNavigate).mockReturnValue(navigate);
   const { user } = await render(<NotFoundPage />);
 
   // Act: "Home으로 이동" 버튼을 클릭
+  await user.click(screen.getByText('Home으로 이동'));
 
   // Assert: navigate 함수가 '/' 경로와 { replace: true } 옵션으로 호출되었는지 확인
+  expect(navigate).toHaveBeenCalledWith('/', { replace: true });
 });
